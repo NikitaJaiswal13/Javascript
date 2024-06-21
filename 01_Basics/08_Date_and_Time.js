3c9ba1b0330e3db22614b810baad49c80f0f03dc
nikitajaiswal13 <nikkijaiswal535@gmail.com>
2024-06-21 15:00:19 +0530

    Array

--- 01_Basics/08_Date_and_Time.js
+++ 01_Basics/08_Date_and_Time.js
@@ -25,3 +25,5 @@ let newDate = new Date()
 console.log(newDate);
 console.log(newDate.getMonth() + 1);
 console.log(newDate.getDay());
+
+
---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,28 @@
+// Ways to declare an Array
+
+const Arr = [0 , 1 , 2 , 3 , 4 , 5 ]
+const My_Arr = new Array(0 , 1 , 2 , 3 , 4 ,5)
+
+// Array Methods
+
+const myArr = [0 , 1 , 2 , 3 , 4 , 5]
+
+myArr.push(6) // It will add 6 in the original array
+myArr.pop() //It will remove the last element in the array
+
+myArr.unshift(9) //It will add the element at the starting of the array
+myArr.shift() // Removes the first element in the array
+
+myArr.includes(5) // Returns true if element is present in the array
+myArr.indexOf(3) // Returns the index at which the element is present
+myArr.join() // It changes the typeof array to string
+
+console.log(myArr)
+
+//slice and Splice
+
+console.log(myArr.slice(1,3))
+console.log(myArr)
+
+console.log(My_Arr.splice(1,3))
+console.log(myArr)

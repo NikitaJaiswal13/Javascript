c465b650a0256f5bbcbd081f6d9055a26df84341
nikitajaiswal13 <nikkijaiswal535@gmail.com>
2024-06-23 14:40:14 +0530

    Objects

--- 02_Basics/05_Objects.js
+++ 02_Basics/05_Objects.js
@@ -8,18 +8,10 @@ let user = {
 
 //Object destructure => This syntax allows you to pull values directly from an object and bind them to variables using a concise and readable form
 
-<<<<<<< HEAD
 //In this case we do not need to use user.course_name we can directly use course_name
 
 let {course_name} = user 
-console.log(course_name)
+console.log(course_name) 
 
 // My Github API
 // https://api.github.com/users/NikitaJaiswal13
-=======
-let {course_name} = user
-console.log(course_name)
-
-// My Github API
-// https://api.github.com/users/NikitaJaiswal13
->>>>>>> 06dd92e2ef40130d9103277c5ad77fc759bb17ba

3cbabdbd6e1c3aeaf129e1c719e7e79f799702f7
nikitajaiswal13 <nikkijaiswal535@gmail.com>
2024-06-23 13:12:54 +0530

    Function

---
+++ 03_Basics/01_Functions.js
@@ -0,0 +1,31 @@
+function my_name(){
+    console.log("My name is Nikita Jaiswal")
+}
+
+function addTwonumber(number1 , number2){
+    return number1 + number2
+}
+
+let result = addTwonumber(4 , 5)
+console.log(result)
+
+// my_name // This is just refernce of the function 
+my_name() 
+
+// While function definition => Parameter
+// While function call => Arguments
+// If not passed any argument , then the value is undefined 
+
+function loggedIn(username = "nj") {
+
+    if(username)
+        console.log(`${username} logged in successfully !`)
+
+    else    
+        console.log('Error while getting username !')
+
+    return
+}
+
+loggedIn()
+loggedIn("Nikita")

7413280a0076e6308f2176a3ef1a433b90db3845
nikitajaiswal13 <nikkijaiswal535@gmail.com>
2024-07-03 22:29:02 +0530

    Loops

---
+++ 03_Basics/09_WhileLoop.js
@@ -0,0 +1,14 @@
+let index = 0
+
+while (index < 10) {
+    index++
+    console.log(`The value of index is ${index}`)
+}
+
+let myArr = ["Pen" , "Pencil" , "Protactor" , "Compass"]
+
+let i = 0
+while (i < myArr.length) {
+    i++
+    console.log(`The value of my array is ${myArr[i]}`)
+}
---
+++ 03_Basics/10_DoWhileLoop.js
@@ -0,0 +1,25 @@
+
+let i = 0
+
+do {
+  //console.log(`The value is ${i}`)
+  i++    
+} while (i<10);
+
+// let myArr = ["Hi" , "Hello" , "Bye" , "Good-Bye"]
+
+// let j = 0
+
+// do {
+//     console.log(`The value of my array is ${myArr[j]}`)
+//     j++
+// } while (j < myArr.length)
+
+    let myArr = ["Hi", "Hello", "Bye", "Good-Bye"];
+
+let j = 0;
+
+do {
+    console.log(`The value of my array is ${myArr[j]}`);
+    j++;
+} while (j < myArr.length);

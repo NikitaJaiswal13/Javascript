db7192758102d5f11caf9adb4e9b5d87beb4a417
nikitajaiswal13 <nikkijaiswal535@gmail.com>
2024-07-08 12:04:54 +0530

    Other Loops

---
+++ 03_Basics/11_ForOf.js
@@ -0,0 +1,16 @@
+const arr = [1 , 2 , 3 , 4 , 5]
+
+for (const num of arr) {
+    console.log(num)
+}
+
+// Object is not iterable
+
+// const myObject = {
+//     name : "Nikita Jaiswal",
+//     age : 18
+// }
+
+// for (const it of myObjectbject) {
+//     console.log(it);
+// }
---
+++ 03_Basics/12_ForIn.js
@@ -0,0 +1,21 @@
+const arr = [ 10 , 20 , 30 , 40 , 50]
+
+for (const key in arr) {
+//    console.log(key)
+    // console.log(arr[key])
+}
+//ForIn loop in array returns the index of the array
+
+const myObject = {
+    Name : "Nikita Jaiswal" ,
+    Age : 18 ,
+    College : "BVCOEW"
+}
+
+for (const key in myObject) {
+//   console.log(key)
+//   console.log(myObject[key])
+}
+
+
+//for Array ForOf Loop is used , for Object FoeIn loop is used

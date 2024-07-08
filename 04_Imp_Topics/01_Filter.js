809d4b0452130bb1c5024cad928a5f5974426ad1
nikitajaiswal13 <nikkijaiswal535@gmail.com>
2024-07-08 19:46:52 +0530

    Map , Filter and  Reduce

--- 04_Imp_Topics/01_Filter.js
+++ 04_Imp_Topics/01_Filter.js
@@ -16,7 +16,7 @@ let mynums = [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
 let new_nums = mynums.filter( (item) => item > 3)
 
 new_nums = mynums.filter( (item) => {
-    return item > 4
+    return item > 7
 })
 console.log(new_nums)
 

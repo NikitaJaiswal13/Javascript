bb121af887a61a5314d1797afcf5c93da5432e73
nikitajaiswal13 <nikkijaiswal535@gmail.com>
2024-07-08 19:45:14 +0530

    Map , Filter and  Reduce

---
+++ 04_Imp_Topics/02_Map.js
@@ -0,0 +1,15 @@
+let myNums1 = [1, 2, 3, 4, 5] 
+
+let map1 = myNums1.map( (item) => {
+    return item+10
+})
+console.log(map1);
+
+// Chaining
+
+let myNums2 = [1, 2, 3, 4, 5] 
+let map2 = myNums2
+        .map( (item) => item+1)
+        .map( (item) => item-1)
+        .filter( (item) => item>=3)
+console.log(map2);
---
+++ 04_Imp_Topics/03_Reduce.js
@@ -0,0 +1,22 @@
+const arr1 = [ 11 , 12 , 13 , 14 , 15 ]
+
+const red1 = arr1.reduce( function (acc , curval) {
+    return acc + curval  
+} , 0)
+console.log(red1)
+
+const red2 = arr1.reduce( (acc , curval) => (acc+curval) , 0)
+console.log(red2)
+
+// Reduce is usually used when we make e-commerce website
+
+//++++++++++++++++++++++++++++++++++++++++++++++ More Information +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// map , filter and reduce are higher-order functions
+
+// Map :- The map function applies a given function to each item of an iterable (e.g., a list) and returns a list of the results. It does not modify the original iterable.
+
+// Filter :- The filter function constructs an iterator from elements of an iterable for which a function returns true. Essentially, it filters out the elements that do not satisfy the condition defined by the function.
+
+// Reduce :- The reduce function applies a rolling computation to sequential pairs of values in a list. It uses a function that takes two arguments and combines them into a single value, and then applies this function cumulatively to the items of the iterable.
+

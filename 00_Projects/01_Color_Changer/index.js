1280e2c63699151246335786d5cbeb9b3c34f309
nikitajaiswal13 <nikkijaiswal535@gmail.com>
2024-07-13 10:36:47 +0530

    Projects

--- 00_Projects/01_Color_Changer/index.js
+++ 00_Projects/01_Color_Changer/index.js
@@ -3,15 +3,15 @@ const button = document.querySelectorAll('button')
 
 button.forEach( function (button) {
     button.addEventListener('click' , function(e){
-        console.log(e)
-        console.log(e.target)
+        // console.log(e)
+        // console.log(e.target)
         if (e.target.id === 'green') {
             body.style.backgroundColor = e.target.id;
           }
           if (e.target.id === 'white') {
             body.style.backgroundColor = e.target.id;
           }
-          if (e.target.id === 'red') {
+          if (e.target.id == 'blue') {
             body.style.backgroundColor = e.target.id;
           }
           if (e.target.id === 'yellow') {
---
+++ 00_Projects/03_Digital_Clock/index.js
@@ -0,0 +1,11 @@
+const time = document.querySelector('#clock')
+
+const date = new Date()
+console.log(date.toLocaleTimeString())
+
+setInterval( function(){
+  const date = new Date()
+  // console.log(date.toLocaleTimeString())
+  // clock.innerHTML = 'date.toLocaleTimeString()'
+  clock.innerHTML = date.toLocaleTimeString();
+} , 1000)
